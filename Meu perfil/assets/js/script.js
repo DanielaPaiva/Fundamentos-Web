5a1727b3ca2b2a07fe0e3b1e15fcc77039f41a9a
unknown <danielapaiva386@gmail.com>
2020-08-18 16:51:43 -0300

    Alterações com js

---
+++ Meu perfil/assets/js/script.js
@@ -0,0 +1,41 @@
+let nome=document.getElementById('nome')
+let email=document.querySelector('#email')
+let assunto=document.querySelector('#assunto')
+
+function validaNome(){
+    let txtNome=document.querySelector("#txtNome")
+
+    if(nome.value.length < 3){
+        txtNome.innerHTML="Nome inválido"
+        txtNome.style.color='red'
+    }else{
+        txtNome.innerHTML=" "
+  
+    }
+}
+
+function validaEmail(){
+    let txtEmail=document.querySelector('#txtEmail')
+
+    if(email.value.indexOf("@")== -1 || email.value.indexOf(".")==-1){
+        txtEmail.innerHTML="Email inválido"
+        txtEmail.style.color='red'
+    }else{
+        txtEmail.innerHTML=" "
+      
+    }
+}
+
+function validaTel(){
+    let txtTel=document.querySelector('#txtTel')
+
+    if(telefone.value.length>11){
+        txtTel.innerHTML="Telefone inválido"
+        txtTel.style.color='red'
+    }else{
+        txtTel.innerHTML=""
+        
+    }
+
+}
+
